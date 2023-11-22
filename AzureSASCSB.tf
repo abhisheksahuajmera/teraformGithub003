cc57b7d69ed8236c0f9d7ce275efa75e93bf96fd
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-22 14:44:42 +0530

    Resolved Error-Apply-01

--- AzureSASCSB.tf
+++ AzureSASCSB.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "rg103tg" {
   location = "east us"
 }
 
-resource "azurerm_storage_account" "sa102tg" {
-  name                     = "sa102"
+resource "azurerm_storage_account" "sa103tg" {
+  name                     = "sa103"
   resource_group_name      = azurerm_resource_group.rg103tg.name
   location                 = azurerm_resource_group.rg103tg.location
   account_tier             = "Standard"
@@ -13,13 +13,13 @@ resource "azurerm_storage_account" "sa102tg" {
 
 resource "azurerm_storage_container" "sc101tg" {
   name                  = "sc101"
-  storage_account_name  = azurerm_storage_account.sa102tg.name
+  storage_account_name  = azurerm_storage_account.sa103tg.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "ab101tg" {
   name                   = "samples.txt"
-  storage_account_name   = azurerm_storage_account.sa102tg.name
+  storage_account_name   = azurerm_storage_account.sa103tg.name
   storage_container_name = azurerm_storage_container.sc101tg.name
   type                   = "Block"
   source                 = "samples01.txt"
